f43c5ccee282bd444fbf8e58e6d786a8c0b9033a
Mahdi Bagheri <qarjis8@gmail.com>
2024-04-14 21:17:34 +0330

    update URL

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://tarkhineh-z7r2.onrender.com",
+  baseURL: "https://tarkhin.netlify.app/",
 });
 
 const fetchProducts = async () => {
