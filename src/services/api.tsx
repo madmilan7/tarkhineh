254ac062bdbdb26f67fc51f046061c95d6a5f5ee
Mahdi Bagheri <qarjis8@gmail.com>
2024-04-15 12:31:07 +0330

    update URL

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://tarkhineh-1.vercel.app",
 });
 
 const fetchProducts = async () => {
